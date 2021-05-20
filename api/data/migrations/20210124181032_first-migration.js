98074e2a9b38a35f9290ed62163f2395d727879e
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-20 18:37:24 -0500

    node-server first commit and set up

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,54 @@
-exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("artists", (tbl) => {
+      tbl.increments("artist_id");
+      tbl.string("artist_name", 50).notNullable().unique();
     })
-}
-
-exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+    .createTable("albums", (tbl) => {
+      tbl.increments("album_id");
+      tbl.string("album_name", 50).notNullable().unique();
+    })
+    .createTable("genres", (tbl) => {
+      tbl.increments("genre_id");
+      tbl.string("genre_type", 25).notNullable().unique();
+    })
+    .createTable("songs", (tbl) => {
+      tbl.increments("song_id");
+      tbl.string("song_name").notNullable().unique();
+      tbl
+        .integer("genre_id")
+        .unsigned()
+        .notNullable()
+        .references("genre_id")
+        .inTable("genres");
+      tbl
+        .integer("album_id")
+        .unsigned()
+        .notNullable()
+        .references("album_id")
+        .inTable("albums");
+    })
+    .createTable("song_artists", (tbl) => {
+      tbl.increments("association_id");
+      tbl
+        .integer("song_id")
+        .unsigned()
+        .notNullable()
+        .references("song_id")
+        .inTable("songs");
+      tbl
+        .integer("artist_id")
+        .unsigned()
+        .notNullable()
+        .references("artist_id")
+        .inTable("artists");
+    });
+};
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("song_artists")
+    .dropTableIfExists("songs")
+    .dropTableIfExists("genres")
+    .dropTableIfExists("albums")
+    .dropTableIfExists("artists");
+};
