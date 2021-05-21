a58178eaaf2e4bf64739148f096f687154371655
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-20 21:26:42 -0500

    node-server tests started

--- api/__tests__/server.test.js
+++ api/__tests__/server.test.js
@@ -1,25 +1,69 @@
-const request = require('supertest')
-const server = require('../server')
-const db = require('../data/db-config')
+const db = require("../data/db-config");
+const Songs = require("../songs/songs-model");
+const { clean } = require("knex-cleaner");
+const server = require("../server");
+const request = require("supertest");
+
+const first = {
+  song_name: "Lonely",
+  artist_name: "Akon",
+  album_name: "Lonely",
+  genre_type: "RnB",
+};
+
+const second = {
+  song_name: "Mr.Brightside",
+  artist_name: "The Killers",
+  album_name: "Hot Fuss",
+  genre_type: "Alternative Rock",
+};
+
+const expected = [
+  {
+    song_name: "Lonely",
+    artist_name: "Akon",
+    album_name: "Lonely",
+    genre_type: "RnB",
+  },
+  {
+    song_name: "Mr.Brightside",
+    artist_name: "The Killers",
+    album_name: "Hot Fuss",
+    genre_type: "Alternative Rock",
+  },
+];
 
 beforeAll(async () => {
-  await db.migrate.rollback()
-  await db.migrate.latest()
-})
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
 beforeEach(async () => {
-  await db.seed.run()
-})
-afterAll(async (done) => {
-  await db.destroy()
-  done()
-})
+  await clean(db, {
+    mode: "truncate",
+    ignoreTables: ["knex_migrations", "knex_migrations_lock"],
+  });
+});
+afterAll(async () => {
+  await db.destroy();
+});
 
-it('sanity check', () => {
-  expect(true).not.toBe(false)
-})
-
-describe('server.js', () => {
-  it('is the correct testing environment', async () => {
-    expect(process.env.NODE_ENV).toBe('testing')
-  })
-})
+describe("[GET] request works", () => {
+  beforeEach(async () => {
+    await Songs.create(first);
+    await Songs.create(second);
+  });
+  test("request comes back with seed data", async () => {
+    const res = await request(server).get("/api/songs");
+    console.log(res.body);
+    expect(res.body).toMatchObject(expected);
+  });
+});
+// describe('[POST] request works', () => {
+//   beforeEach(()=>{
+//     await Songs.create(listOfSongs)
+//   })
+//   test('request comes back with seed data', () => {
+//     const res = request(server).get('/hobbits')
+//     expect(res.body).toMatchObject
+//   })
+// })
